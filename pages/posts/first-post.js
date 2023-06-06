20b8cc539c1ad6f547d92bfce2f0dd9956d8128b
Chen ZiLu <2822459649@qq.com>
2023-06-06 17:13:51 +0800

    init

---
+++ pages/posts/first-post.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from '../../styles/Home.module.css';
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script"
+export default function FirstPost() {
+
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+            />
+            <h1>First Post</h1>
+            <h1 >
+                <Link href="/">Back to home</Link>
+            </h1>
+            <Image
+            src="/images/profile.jpg"
+            height={144}
+            width={144}
+            alt="erjwerj"
+            
+            />
+        </>
+    )
+}
